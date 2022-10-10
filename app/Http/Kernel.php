93891610247e0b43e516722bb87a43d1baa6313e
Andrey <mikhalev.a.y@gmail.com>
2022-10-10 12:31:46 +0600

    small fix of Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -3,7 +3,7 @@
 namespace App\Http;
 
 use App\Http\Middleware\ActionIsAllowed;
-use App\Http\Middleware\checkUserPermission;
+use App\Http\Middleware\CheckUserPermission;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -65,7 +65,7 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'checkUserPermission' => checkUserPermission::class,
+        'checkUserPermission' => CheckUserPermission::class,
         'actionIsAllowed' => ActionIsAllowed::class
     ];
 }
