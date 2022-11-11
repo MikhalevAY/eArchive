242a19ed9021ffb6a575a9b2992da91cc70d34db
Andrey <mikhalev.a.y@gmail.com>
2022-11-11 15:40:35 +0600

    fix logo on auth

--- resources/views/auth/index.blade.php
+++ resources/views/auth/index.blade.php
@@ -5,27 +5,26 @@
 @section('content-class', 'center')
 
 @section('content')
-<div class="login-form">
-    <img src="/storage/{{ $companyLogo }}" alt="" draggable="false">
-    <h2>@yield('title')</h2>
-    <h4>Добро пожаловать! Войдите в свой аккаунт.</h4>
-    <form action="{{ route('login') }}" method="post">
-        @csrf
-        <div class="row">
-            <span>Логин</span>
-            <input type="text" name="email" placeholder="Логин" autocomplete="off" />
-        </div>
-        <div class="row">
-            <span>Пароль</span>
-            <input type="password" name="password" placeholder="••••••" autocomplete="off"/>
-            <div class="eye"></div>
-        </div>
-        <p class="text-right">
-            <a href="{{ route('remindPassword') }}">Забыли пароль?</a>
-        </p>
-        <input type="submit" value="Авторизоваться"/>
-        <div class="result text-center"></div>
-    </form>
-</div>
-<div class="login-form-ball"></div>
+    <div class="login-form">
+        <h2>@yield('title')</h2>
+        <h4>Добро пожаловать! Войдите в свой аккаунт.</h4>
+        <form action="{{ route('login') }}" method="post">
+            @csrf
+            <div class="row">
+                <span>Логин</span>
+                <input type="text" name="email" placeholder="Логин" autocomplete="off"/>
+            </div>
+            <div class="row">
+                <span>Пароль</span>
+                <input type="password" name="password" placeholder="••••••" autocomplete="off"/>
+                <div class="eye"></div>
+            </div>
+            <p class="text-right">
+                <a href="{{ route('remindPassword') }}">Забыли пароль?</a>
+            </p>
+            <input type="submit" value="Авторизоваться"/>
+            <div class="result text-center"></div>
+        </form>
+    </div>
+    <div class="login-form-ball"></div>
 @endsection
